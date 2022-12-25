2451a72b3867ba5f636a1747001b9d2542edf184
Yohey373 <load5123874@gmail.com>
2022-12-25 12:46:54 +0900

    DungeonMemoryManagerを追加 PlayerParameterBaseにMemoryの読み取りを追加

--- Assets/Scripts/PlayerParameterBase.cs
+++ Assets/Scripts/PlayerParameterBase.cs
@@ -16,4 +16,22 @@ public class PlayerParameterBase : CharacterParameterBase
         base.maxHitPoint = base.HitPoint;
         base.AttackPoint = playerAttackPoint;
     }
+
+    private void Start()
+    {
+        // ƒ_ƒ“ƒWƒ‡ƒ“‚Ì‹L‰¯‚ª‚ ‚é‚Ì‚Å‚»‚±‚©‚çhitpoint‚ðŽæ“¾‚·‚é
+        if (DungeonMemoryManager.Instance.GetplayerHitPoint != 0)
+        {
+            base.HitPoint = DungeonMemoryManager.Instance.GetplayerHitPoint;
+            base.maxHitPoint = DungeonMemoryManager.Instance.GetPlayerMaxHitPoint;
+            base.AttackPoint = DungeonMemoryManager.Instance.GetPlayerAttackPoint;
+        }
+        else
+        {
+            base.HitPoint = playerHitPoint;
+            base.maxHitPoint = base.HitPoint;
+            base.AttackPoint = playerAttackPoint;
+        }
+    }
+
 }
